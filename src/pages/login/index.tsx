846add8b6be6f49ac4dfe9edd867d0abd51a8a37
Augusto Pires <mpires.augusto@gmail.com>
2023-08-15 17:44:24 -0300

    feat: adicionados context e routes

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,17 @@
+
+
+function Login() {
+
+
+
+  return(
+    <form action="">
+      <p>Faça seu login:</p>
+      <input type="email" id="email" alt="Digite seu email cadastrado" placeholder="E-mail"/>
+      <input type="password" id="" alt="Digite sua senha" placeholder="Senha"/>
+      <input type="submit" id="submit" />
+    </form>
+  )
+}
+
+export {Login};
