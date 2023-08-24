099c0bdc701d81b5493b1aa608812c2a7848d052
Augusto Pires <mpires.augusto@gmail.com>
2023-08-24 03:30:30 -0300

    feat: adicionadas mais opções de filtro

--- src/pages/productList/index.tsx
+++ src/pages/productList/index.tsx
@@ -17,11 +17,20 @@ function ProductList() {
   if(name === "" && type === "no-filter"){
   return setMedList(products)
   }
-  else if(type === "no-filter") {
-    updatedList = updatedList.filter((med) => {
-    return med.name?.toLowerCase().includes(name.toLowerCase())})
-    return setMedList(updatedList);
+  else if(type === "alpha"){
+    updatedList = updatedList.sort((a, b) => (a.name! > b.name!) ? 1 : -1)
+    return setMedList(updatedList)
+  } else if(type === "rise") {
+    updatedList = updatedList.sort((a, b) => a.price! - b.price!)
+    return setMedList(updatedList)
+  } else if(type === "desc") {
+    updatedList = updatedList.sort((a, b) => b.price! - a.price!)
+    return setMedList(updatedList)
   }
+    else if(type === "no-filter") {
+      updatedList = updatedList.filter((med) => {
+      return med.name?.toLowerCase().includes(name.toLowerCase())})
+      return setMedList(updatedList);} 
     updatedList = updatedList.filter((med) => {
     return med.isControlled?.includes(type) && med.name?.toLowerCase().includes(name.toLowerCase())
   });
@@ -55,6 +64,9 @@ if(products.length === 0) {
             <option value="no-filter">Todos</option>
             <option value="false">Comum</option>
             <option value="true">Controlado</option>
+            <option value="alpha">Ordem Alfabética</option>
+            <option value="rise">Preço crescente</option>
+            <option value="desc">Preço decrescente</option>
           </select>
           <input 
           type="text" 
@@ -64,7 +76,7 @@ if(products.length === 0) {
           />
         </form>
       </section>
-      <section>
+      <section style={{display: "flex", justifyContent: "space-around"}}>
         <MedCards meds={medList}/>
       </section>
     </main>
