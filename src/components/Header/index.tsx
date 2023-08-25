aa59ac42b8eb4cc3c3454297e12135a6444e22bc
Augusto Pires <mpires.augusto@gmail.com>
2023-08-24 22:46:55 -0300

    feat: estilização do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import {PageHeader, StyledA, StyledLi, StyledUl} from './styled'
+import {PageHeader, StyledA, StyledLi, StyledUl,StyledImg} from './styled'
 
 function Header() {
   return(
@@ -7,20 +7,17 @@ function Header() {
         <StyledA href='/'>
           <StyledLi>Início</StyledLi>
         </StyledA>
+        <StyledA href='/medicamentos'>
+          <StyledLi>Medicamentos</StyledLi>
+        </StyledA>
+        <StyledA href='/'>
+          <StyledImg/>
+        </StyledA>
         <StyledA href='/cadastro-farmacia'>
           <StyledLi>Nova Farmácia</StyledLi>
         </StyledA>
         <StyledA href='/cadastro-medicamento'>
           <StyledLi>Novo Medicamento</StyledLi>
-        </StyledA>
-        <StyledA href='/mapa-lojas'>
-          <StyledLi>Mapa</StyledLi>
-        </StyledA>
-        <StyledA href='/medicamentos'>
-          <StyledLi>Medicamentos</StyledLi>
-        </StyledA>
-        <StyledA href='/registro'>
-          <StyledLi>Registro</StyledLi>
         </StyledA>
       </StyledUl>
     </PageHeader>
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,37 +1,64 @@
 import {styled} from 'styled-components';
 
 const PageHeader = styled.header`
+  box-sizing: border-box;
+  background-color: #ffffff;
+  box-shadow: 1px 1px 10px rgba(0, 0, 255, 0.3);
   display: flex;
   justify-content: space-around;
   align-items: center;
   width: 100%;
-  height: 3rem;
-  border: 2px grey;
-  border-style: dashed;
-  margin-bottom: 2rem;
+  height: 4rem;
+  margin-bottom: 1rem;
+  overflow: hidden;
 `
 
 const StyledUl = styled.nav`
+margin: 0 3rem;
   display: flex;
   list-style: none;
   justify-content: space-between;
+  align-items: center;
   width: 100%;
 `
 const StyledA = styled.a`
-  margin: 2rem;
+text-decoration: none;
+display: flex;
+align-items: center;
+justify-content: center;
+&:last-of-type{
+  margin-right: 1.75rem;
+}
 `
 
 const StyledLi = styled.li`
-  background: black;
-  color: white;
+  text-transform: uppercase;
+  line-height: 1.35rem;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  -webkit-text-stroke: 1px blue;
+  height: 4rem;
+  color: rgb(200,200,255);
+  font-size: 1.25rem;
+  font-weight: 600;
   text-decoration: none;
-  border: 2px grey solid;
-  padding: 0.5rem;
+  width: 10rem;
   &:hover {
-    background: grey;
+    color: blue;
+    text-shadow: 0px 0px 30px rgba(0,0,255,0.5);
+    transition: 0.25s;
   }
 `
+const StyledImg = styled.span`
+background: url("/img/pharm-logo-sm.png") no-repeat center;
+background-size: 100% 90%;
+width: 10rem;
+height: 4rem;
+height: 3.5;
+`
 
-export {PageHeader, StyledUl, StyledA, StyledLi}
+export {PageHeader, StyledUl, StyledA, StyledLi, StyledImg}
 
 //
